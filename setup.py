9e9a6d296cdb3b6028d001811c51482cb7dd18a0
Riccardo Viviano <40870708+VivianoRiccardo@users.noreply.github.com>
2021-11-13 00:04:13 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ with open("README.md", 'r') as f:
 
 setup(
     name="Pyllab",
-    version="0.0.1",
+    version="0.0.2",
     packages=find_packages(),
     author="Riccardo Viviano, Matteo Galdi",
     description="Deep learning library",
