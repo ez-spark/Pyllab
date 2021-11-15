da05d73fca0c83c5913f68c5d5793fdaefec00a7
Riccardo Viviano <40870708+VivianoRiccardo@users.noreply.github.com>
2021-11-15 10:57:54 +0100

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 from setuptools import Extension, setup, find_packages
 from Cython.Build import cythonize
+from setuptools.command.build_ext import build_ext
 
 try:
     from wheel.bdist_wheel import bdist_wheel as _bdist_wheel
@@ -13,16 +14,33 @@ except ImportError:
 with open("README.md", 'r') as f:
     long_description = f.read()
 
+link_args = ['-static-libgcc',
+             '-static-libstdc++',
+             '-Wl,-Bstatic,--whole-archive',
+             '-lwinpthread',
+             '-Wl,--no-whole-archive',
+             '-L .']
+
+class Build(build_ext):
+    def build_extensions(self):
+        if self.compiler.compiler_type == 'mingw32':
+            for e in self.extensions:
+                e.extra_link_args = link_args
+        super(Build, self).build_extensions()
 
 setup(
     name="Pyllab",
     version="0.0.2",
+    cmdclass={'build_ext': Build},
     packages=find_packages(),
+    setup_requires=['wheel'],
     author="Riccardo Viviano, Matteo Galdi",
     description="Deep learning library",
     long_description=long_description,
     long_description_content_type='text/markdown',
     url="https://github.com/ez-spark/Pyllab",
-    ext_modules = cythonize([Extension("llab", ["Pyllab/__init__.pyx"],libraries=["llab"])]),
+    ext_modules = cythonize([Extension("llab", ["Pyllab/__init__.pyx"],libraries=["llab"],extra_link_args=["-DSOME_DEFINE_OPT", "-L . "],extra_compile_args=["-O3","-mavx2"])]),
+    
+
 )
 
